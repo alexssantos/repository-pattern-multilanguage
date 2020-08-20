3a2c470c9838c6e4d985fc2ec4c04711d8869746
Alex Santos <arkanjo.alex@gmail.com>
2020-08-20 15:03:03 -0300

    Configuração de acesso ao banco

--- c-sharp/repository-pattern/repository-pattern.api/Startup.cs
+++ c-sharp/repository-pattern/repository-pattern.api/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using repository_pattern.repository.Context;
 
 namespace repository_pattern.api
 {
@@ -24,6 +20,10 @@ namespace repository_pattern.api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<MyContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("MyConnString")
+			));
+
 			services.AddControllers();
 		}
 
