83d36c1f1b6884466951ae1b3b0624f50719fd29
Alex Santos <arkanjo.alex@gmail.com>
2020-08-20 13:57:25 -0300

    Projeto Base c-sharp - API - Repository - Tests

---
+++ c-sharp/repository-pattern/repository-pattern.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace repository_pattern.tests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
